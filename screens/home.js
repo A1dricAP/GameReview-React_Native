2cab4a848aa3b114dcecccb07886c7a8958da57c
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-10-28 21:06:49 +0530

    #29

--- screens/home.js
+++ screens/home.js
@@ -1,8 +1,13 @@
 import React, {useState}  from "react";
-import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native'
+import {StyleSheet, View, Text, FlatList, TouchableOpacity, Modal} from 'react-native'
 import Card from '../shared/card'
+import {MaterialIcons} from '@expo/vector-icons'
+
 
 export default function Home({navigation}){
+
+    const [modalOpen, setmodalOpen]=useState(false)
+
     const [review, setreview]=useState([
         {
             title:'zelda', rating:5, body:'lorem ipsum', key:'1'
@@ -17,6 +22,26 @@ export default function Home({navigation}){
 
     return(
         <View style={styles.container}>
+
+            <Modal visible={ modalOpen } animationType='slide'>
+                <View style={styles.modalContent}>
+                <MaterialIcons
+                  name='close'
+                  size={24}
+                  onPress={()=>setmodalOpen(false)}
+                  style={styles.modalT}
+            />
+                    <Text>Hello, from Modal!</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+            name='add'
+            size={24}
+            onPress={()=>setmodalOpen(true)}
+            style={styles.modalToggle}
+            />
+
            <FlatList data={review}
            renderItem={({item})=>( //here item is the individual item of review object defined in the data prop.
             //sending item as the second parameter to be retrieved in Reviewdetails file. 
@@ -35,7 +60,29 @@ const styles=StyleSheet.create({
      container:{
         //  backgroundColor:'',
          padding:24,
-     
+     },
+     modalContent:{
+         flex:1
+     },
+     modalToggle:{
+         marginBottom:10,
+         borderWidth:1,
+         borderColor:'#f2f2f2',
+         padding:10,
+         borderRadius:10,
+         alignSelf:'center'
+     },
+     modalT:{
+         marginTop:20,
+         marginBottom:0,
+         marginBottom:10,
+         borderWidth:1,
+         borderColor:'#f2f2f2',
+         padding:10,
+         borderRadius:10,
+         alignSelf:'center',
+        //  justifyContent:'center'
+        // flexDirection:'column'
      },
      text:{
          padding:5,

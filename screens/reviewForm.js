aa3ab76452c2f750cbd57b7bba0cf9ec8c2c17e6
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-10-29 21:29:53 +0530

    #32

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { StyleSheet, Button, TextInput, View, ActionSheetIOS } from 'react-native';
 import { Formik } from 'formik';
+import * as yup from 'yup'
+
+//creating a schema for the validation of the form. Just like mongoose Schema.
+const ReviewSchema=yup.object({
+  title:yup.string().required().min(4), //using predefined methods from the yup package.
+  body:yup.string().required().min(8),
+  rating:yup.string().required().test('is_num_1-5', 'Rating must be between 1-5', (value)=>{ //this test() takes in a function.
+    return parseInt(value)<6 && parseInt(value)>0
+  })  
+  //like so- test('name_of_the_test', 'feedback message', 'function()')
+}) //sending this function as a validationSchema to formik.
 
 export default function ReviewForm({addReview}) {
 
@@ -9,6 +20,7 @@ export default function ReviewForm({addReview}) {
     <View style={styles.container}>
       <Formik
         initialValues={{ title: '', body: '', rating: '' }}
+        validationSchema={ReviewSchema}
         onSubmit={(values, actions) => {
           actions.resetForm()
           addReview(values)
